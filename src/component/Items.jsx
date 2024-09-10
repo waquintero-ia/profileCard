c5d3606eb0e61cd8aed8d56d596a82afba5162e5
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-09-09 23:06:50 -0500

    add component item

---
+++ src/component/Items.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+
+const Items = ({score, tag}) =>{
+  return(
+    <>
+      <div className="score flex flex-col gap-[9px] text-center leading-[100%]">
+        <div className="number text-[18px] font-bold tracking-wider">
+          {score}
+        </div>
+        <div className="tag text-[10px] tracking-[1.5px]">
+          {tag}
+        </div>
+      </div>
+    </>
+  )
+}
+
+Items.propTypes = {
+  score: PropTypes.string.isRequired, 
+  tag: PropTypes.string.isRequired,      
+};
+
+export default Items
--- src/component/Score.jsx
+++ src/component/Score.jsx
@@ -1,34 +1,28 @@
+import Items from "./Items"
+import score from "../data/score.json"
+
 const Score = () =>{
   return(
     <>
           <div className="score_container flex justify-center">
             <div className="score_content w-[240px] flex gap-[46px]">
               <div className="score_part_1 flex gap-[40px]">
-                <div className="score flex flex-col gap-[9px] text-center leading-[100%]">
-                  <div className="number text-[18px] font-bold tracking-wider">
-                    80K
-                  </div>
-                  <div className="tag text-[10px] tracking-[1.5px]">
-                    Followers
-                  </div>
-                </div>
-                <div className="score flex flex-col gap-[9px] text-center leading-[100%]">
-                  <div className="number text-[18px] font-bold tracking-wider">
-                    803K
-                  </div>
-                  <div className="tag text-[10px] tracking-[1.5px]">
-                    Likes
-                  </div>
-                </div>
+                <Items
+                  key={score[0].tag}
+                  score={score[0].score}
+                  tag={score[0].tag} />
+
+                <Items
+                  key={score[1].tag}
+                  score={score[1].score}
+                  tag={score[1].tag} />
               </div>
-              <div className="score flex flex-col gap-[9px] justify-center text-center leading-[100%]">
-                <div className="number text-[18px] font-bold tracking-wider">
-                  1.4K
-                </div>
-                <div className="tag text-[10px] tracking-[1.5px]">
-                  Photos
-                </div>
-              </div>              
+
+              <Items
+                  key={score[2].tag}
+                  score={score[2].score}
+                  tag={score[2].tag} />
+                          
             </div>
           </div>
     </>
